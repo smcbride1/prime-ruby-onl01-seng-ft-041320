9dbd0661e6ffa9c5a13bf7631acdcd1202157849
Stephen McBride <stevemcbride3@gmail.com>
2020-04-15 21:20:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(integer)
+  return false if integer < 2
+  i = 2
+  while i < integer
+    return false if integer%i == 0
+    i += 1
+  end
+  true
+end
+
+puts prime?(1763)
